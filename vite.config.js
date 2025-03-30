57ca132dca497e6fe17c0ebb48636f305cf8bbb4
Simono18-02 <152652747+Simono18-02@users.noreply.github.com>
2025-03-30 16:35:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nom-de-votre-repo/', // Ajoutez cette ligne
-})
+  base: '/avions/'}
+  )
 
